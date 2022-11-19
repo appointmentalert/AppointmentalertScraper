b6b8b109ced66d49451e712b37ca863a2c85cc0b
ErikOrtmann <saxonaut@gmx.de>
2022-11-19 21:55:19 +0100

    fun stuff

--- main.py
+++ main.py
@@ -33,69 +33,96 @@ def excerpt_data(tables: list):
     return all_free_slots
 
 
-def retrieve_kfzzulassung():
-    browser = Browser(driver_name='chrome')
-    browser.visit('https://www.leipzig.de/fachanwendungen/termine/index.html')
-    browser.driver.switch_to.frame(0)
+def all_time_clicking_wrapper(special_clicking_func):
+    def wrapper(browser, url, tag):
+        browser.visit(url)
+        browser.driver.switch_to.frame(0)
 
-    browser.find_by_name('AGREEMENT_ACCEPT').click()
-    browser.find_by_name('ACTION_INFOPAGE_NEXT').click()
+        browser.find_by_name('AGREEMENT_ACCEPT').click()
+        browser.find_by_name('ACTION_INFOPAGE_NEXT').click()
+
+        special_clicking_func(browser)
+
+        browser.find_by_name("ACTION_CONCERNSELECT_NEXT").click()
+        optional = browser.find_by_name("ACTION_CONCERNCOMMENTS_NEXT")
+        if optional:
+            optional.click()
+        data = excerpt_data(browser.find_by_css('table[class=ekolCalendarMonthTable]'))
+
+        return {tag: data}
+
+    return wrapper
+
+
+@all_time_clicking_wrapper
+def retrieve_kfzzulassung(browser):
     browser.find_by_xpath('//*[@id="id_buergerauswahldienststelle_tree-office"]/tbody/tr[2]/td[2]/div/button').click()
     browser.find_by_id("action_officeselect_termnew_prefix1222936509").click()
     browser.find_by_id("id_1224593434").find_by_xpath("//option[. = '1']").click()
-    browser.find_by_name("ACTION_CONCERNSELECT_NEXT").click()
-    browser.find_by_name("ACTION_CONCERNCOMMENTS_NEXT").click()
-
-    data = excerpt_data(browser.find_by_css('table[class=ekolCalendarMonthTable]'))
-    browser.quit()
-    return data
 
 
-def retrieve_reisegewerbe():
-    browser = Browser(driver_name='chrome')
-    browser.visit('https://www.leipzig.de/fachanwendungen/termine/index.html')
-    browser.driver.switch_to.frame(0)
-
-    browser.find_by_name('AGREEMENT_ACCEPT').click()
-    browser.find_by_name('ACTION_INFOPAGE_NEXT').click()
+@all_time_clicking_wrapper
+def retrieve_reisegewerbe(browser):
     browser.find_by_xpath('//*[@id="id_buergerauswahldienststelle_tree-office"]/tbody/tr[8]/td[2]/div/button').click()
     browser.find_by_id("action_officeselect_termnew_prefix1595996623129").click()
     browser.find_by_id("id_1595996623151").find_by_xpath("//option[. = '1']").click()
-    browser.find_by_name("ACTION_CONCERNSELECT_NEXT").click()
-    browser.find_by_name("ACTION_CONCERNCOMMENTS_NEXT").click()
-
-    data = excerpt_data(browser.find_by_css('table[class=ekolCalendarMonthTable]'))
-    browser.quit()
-    return data
 
 
-def retrieve_aufenthaltstitel():
-    browser = Browser(driver_name='chrome')
-    browser.visit('https://www.leipzig.de/fachanwendungen/termine/abholung-aufenthaltstitel.html')
-    browser.driver.switch_to.frame(0)
+@all_time_clicking_wrapper
+def retrieve_fundbuero(browser):
+    browser.find_by_xpath('//*[@id="id_buergerauswahldienststelle_tree-office"]/tbody/tr[3]/td[2]/div/button').click()
+    browser.find_by_id('action_officeselect_termnew_prefix1459326045557').click()
+    browser.find_by_id('id_1459327477898').find_by_xpath("//option[. = '1']").click()
 
-    browser.find_by_name('AGREEMENT_ACCEPT').click()
-    browser.find_by_name('ACTION_INFOPAGE_NEXT').click()
+
+@all_time_clicking_wrapper
+def retrieve_aufenthaltstitel(browser):
     browser.find_by_css(".buttonTreeviewExpand").click()
     browser.find_by_id("action_officeselect_termnew_prefix1600340740349").click()
     browser.find_by_id("id_1600340740368").click()
     browser.find_by_id("id_1600340740368").find_by_xpath("//option[. = '1']").click()
-    browser.find_by_id("action_concernselect_next").click()
-    browser.find_by_id("action_concerncomments_next").click()
-
-    data = excerpt_data(browser.find_by_css('table[class=ekolCalendarMonthTable]'))
-    browser.quit()
-    return data
 
 
-def post_free_slots(title, data):
+@all_time_clicking_wrapper
+def retrieve_pflichtumtausch(browser):
+    browser.find_by_xpath('//*[@id="id_buergerauswahldienststelle_tree-office"]/tbody/tr[4]/td[2]/div/button').click()  # expand option list
+    browser.find_by_id('action_officeselect_termnew_prefix1317888915').click()  # click button
+    browser.find_by_id('id_1563449415908').find_by_xpath("//option[. = '1']").click()  # select dropdown
+
+
+
+@all_time_clicking_wrapper
+def retrieve_TEMPLATE(browser):
+    browser.find_by_xpath('FILL_ME').click()  # expand option list
+    browser.find_by_id('FILL_ME').click()  # click button
+    browser.find_by_id('FILL_ME').find_by_xpath("//option[. = '1']").click()  # select dropdown
+
+
+def retrieve_all(browser, todos):
+    all_data = {}
+    for todo in todos:
+        func, url, tag = todo[0], todo[1], todo[2]
+        all_data.update(func(browser, url, tag))
+    return all_data
+
+
+def post_free_slots(data):
     http = urllib3.PoolManager()
     http.urlopen('POST', 'http://localhost:8080/assets',
                  headers={'Content-Type': 'application/json'},
-                 body=json.dumps({title:data}))
+                 body=json.dumps(data)
+                 )
 
 
 if __name__ == '__main__':
-    post_free_slots('aufenthaltstitel', retrieve_aufenthaltstitel())
-    post_free_slots('kfz_zulassung', retrieve_kfzzulassung())
-    post_free_slots('reisegewerbe', retrieve_reisegewerbe())
+    url1 = 'https://www.leipzig.de/fachanwendungen/termine/index.html'
+    url2 = 'https://www.leipzig.de/fachanwendungen/termine/abholung-aufenthaltstitel.html'
+    todos = [(retrieve_kfzzulassung, url1, 'kfz_zulassung'),
+             (retrieve_reisegewerbe, url1, 'reisegewerbe'),
+             (retrieve_fundbuero, url1, 'fundbüro'),
+             (retrieve_pflichtumtausch, url1, 'pflichtumtausch_führerschein'),
+             (retrieve_aufenthaltstitel, url2, 'aufenthaltstitel')]
+
+    browser = Browser(driver_name='chrome')
+    post_free_slots(retrieve_all(browser, todos))
+    browser.quit()
